2377311e1c3de46f638da7558d8dc2bb47837c7b
Tobias Schmidt <86082336+crypblockcode@users.noreply.github.com>
2021-08-25 16:25:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const transactions = require('./routes/transactions')
 const assets = require('./routes/assets')
 const accounts = require('./routes/accounts')
