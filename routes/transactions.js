ebc2606245591b5d01752f7f0bba3e428b7064ec
Tobias Schmidt <86082336+crypblockcode@users.noreply.github.com>
2021-08-25 16:26:40 +0200

    Update transactions.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -6,6 +6,16 @@ const indexer_token = "";
 const indexer_server = "http://localhost";
 const indexer_port = 8980;
 
+const transactionsHelper = require("../helpers/transactions")
+
+const algodToken = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+const algodServer = "http://localhost";
+const algodPort = 4001;
+
+let algodClient = new algosdk.Algodv2(algodToken, algodServer, algodPort);
+
+const FAUCET_PASSPHRASE = "boss enemy gift student resource spend garment regret master across gun culture bag sauce pride grace steak general school wheel kiwi cannon upon abstract aisle"
+
 // Instantiate the indexer client wrapper
 let client = new algosdk.Indexer(indexer_token, indexer_server, indexer_port);
 
@@ -32,5 +42,40 @@ router.get("/", async function (req, res) {
 
   res.send(transactions);
 });
+
+// faucet: send 1234 microALGO tokens to a specific account
+router.post("/faucet", async function (req, res) {
+  const receiver = req.body.receiver
+  const faucetAccount = algosdk.mnemonicToSecretKey(FAUCET_PASSPHRASE);
+
+  let params = await algodClient.getTransactionParams().do();
+  params.flatFee = true;
+  params.fee = 1000;
+
+  let note = algosdk.encodeObj("Faucet");
+  let txn = algosdk.makePaymentTxnWithSuggestedParams(
+    faucetAccount.addr,
+    receiver,
+    1234,
+    undefined,
+    note,
+    params
+  );
+
+  let signedTxn = txn.signTxn(faucetAccount.sk);
+  let txId = txn.txID().toString();
+  console.log("Signed transaction with txID: %s", txId);
+
+  await algodClient.sendRawTransaction(signedTxn).do();
+  await transactionsHelper.waitForConfirmation(algodClient, txId, 5);
+
+  res.send({
+    status: 200,
+    id: txId,
+    from: faucetAccount.addr,
+    to: receiver,
+    amount: 1234
+  })
+})
 
 module.exports = router;
